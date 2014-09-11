c3e27119c65342725f9a9542ff65e2fe9ac46541
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-09-10 20:38:06 -0400

    updated for tutorial step 2. constructors and non-static properties

--- BTypeProvider/Library1.fs
+++ BTypeProvider/Library1.fs
@@ -14,7 +14,15 @@ type BusinessObjectTypeProvider (config : TypeProviderConfig) as this =
         let myType = ProvidedTypeDefinition(asm, ns, "MyType", Some typeof<obj>)
         let myProp = ProvidedProperty("MyProperty", typeof<string>, IsStatic = true,
                                         GetterCode = (fun args -> <@@ "Hello world" @@>))
-        myType.AddMember(myProp)
+        myType.AddMember myProp
+        let ctor = ProvidedConstructor([], InvokeCode = fun args -> <@@ "My internal state" :> obj @@>)
+        myType.AddMember ctor
+        let ctor2 = ProvidedConstructor([ProvidedParameter("InnerState", typeof<string>)], 
+                                    InvokeCode = fun args -> <@@ (%%(args.[0]):string):> obj @@>)
+        myType.AddMember ctor2
+        let innerState = ProvidedProperty("InnerState", typeof<string>, 
+                                    GetterCode = fun args -> <@@ (%%(args.[0]) :> obj) :?> string @@>)
+        myType.AddMember innerState
         [myType]
 
     do
